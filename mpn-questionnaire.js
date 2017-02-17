340835cf77af4c5ba0d6661f4eb6273cb253d22b
Shaun Brockhoff <shaun.brockhoff@gdit.com>
2017-02-16 18:38:33 -0600

    Update questionnaire to convert submitted answers to a sample jquery function.

--- mpn-questionnaire.js
+++ mpn-questionnaire.js
@@ -3,7 +3,26 @@
 $(function() {
      $("#mpnForm").on('submit', function(e) {
         event.preventDefault();
-        console.log($(this).serialize());
+        
+        var selections = $(this).serialize();
+         
+        $('#result').html('')
+        .append($(this).serialize())
+        .append('<br />')
+        .append('$( document ).ready(function() {<br/>')
+        .append('&nbsp;&nbsp;$( \'#result\' ).MpnGen({<br/>');
+         
+        $.each(selections.split('&'), function(index, param) {
+            //Only output if ther eis something after the equals
+            if(param.split('=')[1] != '') {
+                 var paramXform = param.replace('=', ': ');
+            
+                $('#result').append('&nbsp;&nbsp;&nbsp;&nbsp;' + paramXform + '<br/>');
+            }
+        });
+        
+        $('#result').append('&nbsp;&nbsp;});<br/>')
+        $('#result').append('});<br/>');
      });
 });
 
