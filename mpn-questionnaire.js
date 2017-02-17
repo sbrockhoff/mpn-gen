0721582f427d2f12f2a2d0689722f9d7b1de3fde
Shaun Brockhoff <Shaun Brockhoff>
2017-02-16 22:32:36 -0600

    Add more content. Clean up some JSLint warnings.

--- mpn-questionnaire.js
+++ mpn-questionnaire.js
@@ -1,28 +1,28 @@
-
-
-$(function() {
-     $("#mpnForm").on('submit', function(e) {
+/*global $ */
+$(function () {
+    'use strict';
+    $("#mpnForm").on('submit', function (e) {
         event.preventDefault();
         
         var selections = $(this).serialize();
          
         $('#result').html('')
-        .append($(this).serialize())
-        .append('<br />')
-        .append('$( document ).ready(function() {<br/>')
-        .append('&nbsp;&nbsp;$( \'#result\' ).MpnGen({<br/>');
+            .append('<br />')
+            .append('$(document).ready(function() {<br/>')
+            .append('&nbsp;&nbsp;$( \'#result\' ).MpnGen({<br/>');
          
-        $.each(selections.split('&'), function(index, param) {
-            //Only output if ther eis something after the equals
-            if(param.split('=')[1] != '') {
-                 var paramXform = param.replace('=', ': ');
+        $.each(selections.split('&'), function (index, param) {
+            //Only output if there is something after the equals
+            if (param.split('=')[1] !== '') {
+                var name = param.substr(0, param.indexOf('=')),
+                    val = param.substr(param.indexOf('=') + 1, param.length);
             
-                $('#result').append('&nbsp;&nbsp;&nbsp;&nbsp;' + paramXform + '<br/>');
+                $('#result').append('&nbsp;&nbsp;&nbsp;&nbsp;' + name + ': \'' + val + '\'<br/>');
             }
         });
         
-        $('#result').append('&nbsp;&nbsp;});<br/>')
+        $('#result').append('&nbsp;&nbsp;});<br/>');
         $('#result').append('});<br/>');
-     });
+    });
 });
 
