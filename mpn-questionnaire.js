6cc12e7c0e86a28559d132f3e6d3ec179afc9ba3
Shaun Brockhoff <sabrockhoff@gmail.com>
2017-02-18 18:28:17 -0600

    Filter out empty input fields and then handle last element in loop correctly

--- mpn-questionnaire.js
+++ mpn-questionnaire.js
@@ -4,20 +4,28 @@ $(function () {
     $("#mpnForm").on('submit', function (e) {
         event.preventDefault();
         
-        var selections = $(this).serialize();
+        /*Filter out empty input fields from form and serialize*/
+        var selections = $('#mpnForm input').filter(function () {
+            return $(this).val();
+        }).serialize(),
+            sel = selections.split('&');
          
+        /*Build JQuery function that will be output to developer*/
         $('#result').html('')
             .append('<br />')
-            .append('$(document).ready(function() {<br/>')
-            .append('&nbsp;&nbsp;$( \'#result\' ).MpnGen({<br/>');
+            .append('$(document).ready(function() {<br />')
+            .append('&nbsp;&nbsp;$( \'#result\' ).MpnGen({<br />');
          
-        $.each(selections.split('&'), function (index, param) {
-            //Only output if there is something after the equals
-            if (param.split('=')[1] !== '') {
-                var name = param.substr(0, param.indexOf('=')),
-                    val = param.substr(param.indexOf('=') + 1, param.length);
+        /*Loop through each selection and add it to the generated JQuery function*/
+        $.each(sel, function (index, param) {
+            var name = param.substr(0, param.indexOf('=')),
+                val = param.substr(param.indexOf('=') + 1, param.length);
             
-                $('#result').append('&nbsp;&nbsp;&nbsp;&nbsp;' + name + ': \'' + val + '\'<br/>');
+            $('#result').append('&nbsp;&nbsp;&nbsp;&nbsp;' + name + ': \'' + val);
+            if (index === (sel.length - 1)) {
+                $('#result').append('\'<br />');
+            } else {
+                $('#result').append('\',<br />');   /*Add comma to all but the last parameter*/
             }
         });
         
